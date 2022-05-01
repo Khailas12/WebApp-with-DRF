40532bb0d26adbfee6bfffed696042e2b88f09d5
Khailas R <khailas303@gmail.com>
2022-05-01 19:31:06 +0530

    updated

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 urlpatterns = [
@@ -7,4 +12,9 @@ urlpatterns = [
 
     path('auth/', include('authentication_app.urls')),
     path('orders/', include('app.urls')),
+    
+    #jwt auth
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
