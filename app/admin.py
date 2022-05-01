6212ba66eab3c2df3791e80893511a1a786d59d8
Khailas R <khailas303@gmail.com>
2022-05-01 18:36:22 +0530

    admin update

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import AppOrder
 
-# Register your models here.
+
+@admin.register(AppOrder)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'size',
+        'order_status',
+        'quantity',
+        'created_at',
+        'updated_at'
+        ]
+    
+    list_filter = [
+        'created_at',
+        'order_status',
+        'size',
+    ]
--- app/models.py
+++ app/models.py
@@ -22,6 +22,7 @@ class AppOrder(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     size = models.CharField(max_length=20, choices=SIZES, default=SIZES[0][0])  
     order_status = models.CharField(max_length=20, choices=ORDER_STATUS, default=ORDER_STATUS[0][0])
+    
     quantity = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
