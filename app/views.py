c0be37b8e83bd99972b34fb07bde0223472d3302
Khailas R <khailas303@gmail.com>
2022-05-11 16:52:54 +0530

    .

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from .models import AppOrder
 from .serializers import AppOrderSeraializer
